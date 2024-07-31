1811e53f9abfdf29b591cdfda32f2f43562ea062
Amer_Morsy <amer.morsy2015@gmail.com>
2024-07-31 14:34:51 +0300

    ui vue setep

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
 import './bootstrap';
+
+import { createApp } from 'vue'
+
+import App from './components/app.vue'
+import router from  './router/index'
+
+createApp(App).use(router).mount("#app")
---
+++ resources/js/components/NotFound.vue
@@ -0,0 +1,7 @@
+
+<template>
+    <h1>Page not found</h1>
+    <hr>
+    <router-link to="/">back to home page</router-link>
+</template>
+
